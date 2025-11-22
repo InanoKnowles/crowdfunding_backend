fa6ac2dbfa819005b69f250ea1a7a0744bea8b7b
InanoKnowles <missinanoknowles@gmail.com>
2025-11-22 12:30:17 +1000

    authentication and permissions

--- crowdfunding/users/views.py
+++ crowdfunding/users/views.py
@@ -5,6 +5,8 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from .models import CustomUser
 from .serializers import CustomUserSerializer
 
@@ -38,3 +40,18 @@ class CustomUserDetail(APIView):
         user = self.get_object(pk) # Grab from database
         serializer = CustomUserSerializer(user) # translates for frontend to read
         return Response(serializer.data)
+    
+class CustomAuthToken(ObtainAuthToken):
+  def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(
+            data=request.data,
+            context={'request': request}
+        )
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.id,
+            'email': user.email
+        })
